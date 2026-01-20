1b095197a4c12eb18f991b657f5126f0fc711a9f
Mudassir Ahmad <ma6386731@gmail.com>
2026-01-20 20:51:07 +0500

    Update JavaScript interview questions

--- function.js
+++ function.js
@@ -52,7 +52,7 @@
 
 // function checkcharactor(str){
 //     let occurances = {}
-//    str.split('').forEach(element => {
+//    str.split(' ').forEach(element => {
 //       if(!occurances.hasOwnProperty(element)){
 //         occurances[element] = 1;
 //       }
@@ -65,3 +65,57 @@
 
 
 // console.log(checkcharactor("mudassir"));
+6 
+// write a js function that clone an array 
+// function cloneArr (arr){
+//     return [...arr]
+// }
+
+// let newarray = cloneArr([1,2,3,4])
+// console.log(newarray);
+7
+// write a javascript function that check the argument typ
+
+// function typeTeller(arg){
+//     console.log(typeof arg);
+// }
+// typeTeller(('mudassir'));
+// typeTeller(([]));
+// typeTeller((89));
+8
+// write a js function that print all the prime number between 1 to 100
+// function isPrime(num, divisor = 2) {
+//     if (num < 2) return false;
+//     if (divisor > Math.sqrt(num)) return true;
+//     if (num % divisor === 0) return false;
+//     return isPrime(num, divisor + 1);
+// }
+
+// function printPrimes(num = 1) {
+//     if (num > 100) return;
+
+//     if (isPrime(num)) {
+//         console.log(num);
+//     }
+
+//     printPrimes(num + 1);
+// }
+
+// printPrimes();
+9 
+// another way to print prime number between 1 to 100
+
+// function printPrimes(n){
+//     for(let i=2; i<=n; i++){
+//         if(n%i === 0){
+//             return false
+//         }
+//     }
+//     return true
+// }
+
+// for(let j=2; j<=100; j++){
+//     if(printPrimes(j)){
+//         console.log(j);
+//     }
+// }
